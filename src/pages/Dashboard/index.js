98690bfcae2ec7baa9e49fd9289c5c329892d4c4
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-10-18 16:15:39 -0400

    create header dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Text } from './styles';
+import { useAuth } from '../../hooks/AuthContext';
+
+import { Container, Header, HeaderContent, Profile, ActionContent } from './styles';
+
+import { FiEdit, FiPower } from 'react-icons/fi';
+// import logoImg from '../../assets/logo.svg';
 
 function Dashboard() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const { signOut, user } = useAuth();
+
+  function createExpense() {
+    // alert('create expense');
+    setIsModalVisible(true);
+  }
+
   return (
     <Container>
-      <Text>Dashboard</Text>
+      <Header>
+        <HeaderContent>
+          {/* <img src={logoImg} alt="" /> */}
+          <Profile>
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <ActionContent>
+            <button tyle="button" onClick={createExpense}>
+              <FiEdit />
+            </button>
+            <button tyle="button" onClick={signOut}>
+              <FiPower />
+            </button>
+          </ActionContent>
+        </HeaderContent>
+      </Header>
+      {isModalVisible && <h1>Modal</h1>}
     </Container>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,6 +3,59 @@ import styled from 'styled-components';
 export const Container = styled.div`
 `;
 
-export const Text = styled.p`
-  color: #000;
+export const Header = styled.header`
+  padding: 32px 0;
+  background: #28262e;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  /* > img { */
+    /* estilizar logo */
+  /* } */
+`;
+
+export const ActionContent = styled.div`
+  margin-left: auto;
+
+  button:first-of-type {
+    svg { color: #ff9000; }
+  }
+
+  button {
+    padding-left: 32px;
+    background: transparent;
+    border: 0;
+
+    svg {
+      color: #999591;
+      width: 20px;
+      height: 20px;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex; 
+  align-items: center;
+  margin-left: 80px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      color: #F4EDE8;
+    }
+
+    strong {
+      color: #ff9000;
+    }
+  }
 `;
