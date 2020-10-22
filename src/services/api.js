53a71c094d8d410903b7c96586423ffd37b71d33
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-10-22 01:53:59 -0400

    config - netlify

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // withCredentials: true,
-  // baseURL: 'http://financemanagerces26back.herokuapp.com',
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://financemanagerces26back.herokuapp.com',
+  // baseURL: 'http://localhost:3333'
 });
 
 export default api;
